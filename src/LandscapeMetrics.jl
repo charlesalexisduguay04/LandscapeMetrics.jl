b097a5317c360601cee1219b1e9f8d58e4d8bcae
tpoisot <timothee.poisot@umontreal.ca>
2025-08-22 15:03:58 -0400

    feat: patch identification

--- src/LandscapeMetrics.jl
+++ src/LandscapeMetrics.jl
@@ -1,12 +1,14 @@
 module LandscapeMetrics
 
 using TestItems
+using StatsBase
 
 include("types.jl")
 export Landscape
 export background
 
-include("identify_patches.jl")
+include("patches.jl")
+export Moore, VonNeumann
 export patches
 
 end # module LandscapeMetrics
--- src/patches.jl
+++ src/patches.jl
@@ -1,4 +1,7 @@
-function patches(l::Landscape; stencil=CartesianIndices((-1:1, -1:1)))
+const VonNeumann = CartesianIndex.([(-1, 0), (0, 0), (0, 1), (0, -1), (1, 0)])
+const Moore = collect(CartesianIndices((-1:1, -1:1)))
+
+function patches(l::Landscape; stencil=Moore)
     # We start by giving each pixel in the landcape its own value, and then we do a little
     # bit of value propagation
     patch_id = collect(reshape(eachindex(l.grid), size(l.grid)))
@@ -11,7 +14,7 @@ function patches(l::Landscape; stencil=CartesianIndices((-1:1, -1:1)))
     has_updated = true
 
     # Now we can loop
-    while(has_updated)
+    while (has_updated)
 
         # We will assume that there is no update yet
         has_updated = false
@@ -37,6 +40,7 @@ function patches(l::Landscape; stencil=CartesianIndices((-1:1, -1:1)))
                     # We can now look at whether all the values are the same
                     current_patch_id = patch_id[inpatch]
 
+
                     # We will now merge the patche
                     if length(unique(current_patch_id)) > 1
                         has_updated = true
@@ -48,7 +52,45 @@ function patches(l::Landscape; stencil=CartesianIndices((-1:1, -1:1)))
 
     end
 
-    # TODO: update the patch ID so that they are ordered by number or cells in the patch
+    # Now we update the values so that the largest patch has ID of 1
+    patch_count = filter(x -> !iszero(x.first), collect(countmap(patch_id)))
+    sort!(patch_count, by=x -> x.second, rev=true)
 
-    return patch_id
+    @info patch_count
+    return_id = zeros(eltype(patch_id), size(patch_id))
+
+    for (i, k) in enumerate(patch_count)
+        return_id[findall(patch_id .== k.first)] .= i
+    end
+
+    return return_id
+end
+
+@testitem "We can correctly identify the patches for a landscape" begin
+    # These are the patches, the nodata value is five
+    V = [
+        1 1 1 1 1 3 3 3 5 5;
+        1 1 1 2 2 2 5 5 5 5;
+        1 1 3 3 3 3 2 2 3 3;
+        1 3 3 3 3 3 2 2 3 3
+    ]
+    l = Landscape(grid=V; nodata=5)
+    # This is what it should look like with the Moore neighborhood
+    P = patches(l; stencil=Moore)
+    M = [
+        1 1 1 1 1 5 5 5 0 0;
+        1 1 1 3 3 3 0 0 0 0;
+        1 1 2 2 2 2 3 3 4 4;
+        1 2 2 2 2 2 3 3 4 4
+    ]
+    @test all(M .== P)
+    # This is what it should look like with the VonNeumann neighborhood
+    P = patches(l; stencil=VonNeumann)
+    M = [
+        1 1 1 1 1 5 5 5 0 0;
+        1 1 1 6 6 6 0 0 0 0;
+        1 1 2 2 2 2 4 4 3 3;
+        1 2 2 2 2 2 4 4 3 3
+    ]
+    @test all(M .== P)
 end
