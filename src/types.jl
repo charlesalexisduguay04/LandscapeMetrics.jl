63b18680b1c8f5d2e1c5c6edb3d233eda2eb3faf
tpoisot <timothee.poisot@umontreal.ca>
2025-08-21 21:55:03 -0400

    doc: typo

--- src/types.jl
+++ src/types.jl
@@ -1,7 +1,7 @@
 """
     AbstractLandscape
 
-This is an abstract class for all landscapes that we will work on -- the overwhelming majority of methods should disaptch on this, and we can build interfaces as required for the more specific types
+This is an abstract class for all landscapes that we will work on -- the overwhelming majority of methods should dispatch on this, and we can build interfaces as required for the more specific types
 """
 abstract type AbstractLandscape end
 
