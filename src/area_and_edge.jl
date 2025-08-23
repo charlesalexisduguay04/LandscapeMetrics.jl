4f8468a6c571c14920e04f92a8826fae941e239f
tpoisot <timothee.poisot@umontreal.ca>
2025-08-22 21:38:59 -0400

    feat?: perimeter

--- src/area_and_edge.jl
+++ src/area_and_edge.jl
@@ -1,4 +1,32 @@
 """
+    perimeter(l::Landscape)
+
+Other keyword arguments are passed to `patches`.
+"""
+function perimeter(l::Landscape, patch; kwargs...)
+    p = patches(l; kwargs...)
+
+    # The perimeter is simply the number of cells that have a different-valued neighbor for
+    # a given patch, but keep in mind that we only look at this for the Von Neumann neighborhood.
+    to_check = filter(i -> i in CartesianIndices(p), findall(isequal(patch), p) .+ VonNeumann)
+
+    # What is very nice here is that we can just count the number of neighbors that have
+    # a different patch value.
+    number_of_edges = p[to_check] .!= patch
+
+    # TODO: the patches that are on the limit of the landscape need to get a perimeter too
+    return nothing
+
+    # The perimeter is therefore the number of edges times the square root of the patch area
+    return number_of_edges * sqrt(l.area)
+end
+
+@testitem "We can measure the perimeter of a patch" begin
+
+end
+
+
+"""
     totalarea(l::Landscape)
 
 Total area of the landscape including the interior background. This is measured as the number of cells that are not part of the exterior background, multiplied by the cell area.
