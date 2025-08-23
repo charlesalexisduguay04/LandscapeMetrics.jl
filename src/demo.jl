5901ff15107c311c7cbd3ada30eb6f3ea24ab07c
tpoisot <timothee.poisot@umontreal.ca>
2025-08-22 21:23:58 -0400

    feat?: demo data for 4/6 situations

--- src/demo.jl
+++ src/demo.jl
@@ -1,4 +1,4 @@
-no_bg_no_border = Landscape(grid=[
+nbnb() = Landscape(grid=[
         1 1 1 1 1 2 2 2 2 4 4 4 4 4;
         1 1 1 3 3 2 2 2 2 4 4 1 1 1;
         1 1 1 3 3 3 3 4 4 4 4 4 4 4;
@@ -10,7 +10,7 @@ no_bg_no_border = Landscape(grid=[
         4 4 4 4 4 4 4 4 2 2 2 2 2 2
     ], nodata=9)
 
-interior_background_no_border = Landscape(grid=[
+ibnb() = Landscape(grid=[
         1 1 1 1 1 2 2 2 2 4 4 4 4 4;
         1 1 1 3 3 2 2 2 2 4 4 1 1 1;
         1 1 1 3 3 3 3 4 4 4 4 4 4 4;
@@ -21,4 +21,16 @@ interior_background_no_border = Landscape(grid=[
         4 4 4 4 4 9 9 9 2 2 2 2 2 2;
         4 4 4 4 4 4 4 4 2 2 2 2 2 2
     ], nodata=9)
+
+function ebnb()
+    g = fill(-9, size(ibnb()) .+ (2, 2))
+    g[2:(end-1), 2:(end-1)] .= replace(ibnb().grid, 9 => -9)
+    return Landscape(grid=g, nodata=9)
+end
+
+function iebnb()
+    g = fill(-9, size(ibnb()) .+ (2, 2))
+    g[2:(end-1), 2:(end-1)] .= ibnb().grid
+    return Landscape(grid=g, nodata=9)
+end
 
