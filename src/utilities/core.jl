8a4f7a828c5e57f358f18df4bf585da18f9f0933
tpoisot <timothee.poisot@umontreal.ca>
2025-08-28 11:42:46 -0400

    feat?: stencil for edge identification

--- src/utilities/core.jl
+++ src/utilities/core.jl
@@ -1,4 +1,8 @@
-function _make_stencil_from_distance()
+function _make_stencil_from_distance(l::Landscape, d::AbstractFloat)
+    # We want to know how many cells are required to get the distance covered. For this, we
+    # simply need to calculate
+    number_of_cells = d / side(l)
+
 end
 
 function corepatches(l::Landscape{T}, edges::Matrix{T}) where {T<:Integer}
