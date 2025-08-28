e763c5fc1ed50372ea3d7df77a7dec05f0727250
tpoisot <timothee.poisot@umontreal.ca>
2025-08-28 13:13:27 -0400

    feat?: edge identification

--- src/utilities/core.jl
+++ src/utilities/core.jl
@@ -1,7 +1,14 @@
 function _make_stencil_from_distance(l::Landscape, d::AbstractFloat)
     # We want to know how many cells are required to get the distance covered. For this, we
     # simply need to calculate
-    number_of_cells = d / side(l)
+    number_of_cells = ceil(Int, d / side(l))
+
+    # We will now generate the initial stencil, which is not yet filtered by distance to the
+    # center of the focal cell
+    span = -number_of_cells:number_of_cells
+    stencil = CartesianIndices((span, span))
+
+    return nothing
 
 end
 
