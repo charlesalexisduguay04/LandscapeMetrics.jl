bf94945c3c7f2ad75c60954b5d8e283217ddf74b
tpoisot <timothee.poisot@umontreal.ca>
2025-08-28 13:06:54 -0400

    bug: unused variable in cellcenters

--- src/utilities/cellcenters.jl
+++ src/utilities/cellcenters.jl
@@ -5,8 +5,6 @@ Returns a matrix of tuples giving the cell center positions in metres. The size 
 """
 function cellcenters(l::Landscape)
 
-    s = side(l)
-
     # We get the centers as a matrix of vectors, as we will need to change the values
     centers = [[idx.I...] for idx in CartesianIndices(l)]
 
@@ -16,7 +14,7 @@ function cellcenters(l::Landscape)
     offset = fill([0.5 .* (size(l) .+ 1)...], size(l))
 
     # We now scale this by the size of the cells
-    return [tuple(x...) for x in (centers .- offset) .* s]
+    return [tuple(x...) for x in (centers .- offset) .* side(l)]
 
 end
 
